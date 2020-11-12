0c0312cf5a8c19f6e53bb536b895f99a1e0f1cef
TChristensenDrumz <tchristensendrumz@gmail.com>
2020-11-11 19:01:13 -0800

    created function that displays weather on button click

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var apiKey = "28f1a1d1d41e500695c02fdd28b46d2a";
 var forecast = $(".col");
+var index = [5, 12, 21, 29, 37];
 
 $("#search").on("click", function(event){
     event.preventDefault();
-
-    var newButton = $("<button class='cities'>").text($("#inputCity").val());
-    $(".buttons").append(newButton);
+    if($("#inputCity").val() !== ""){
+        var newButton = $("<button>").attr("class", "cities").text($("#inputCity").val());
+        $(".buttons").append(newButton);
+    }
 });
 
 function getWeather(city){
@@ -13,13 +15,16 @@ function getWeather(city){
         url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey,
         method: "GET"
     }).then(function(response){
-        console.log(response);
-
         var today = moment().format("(MM/D/YYYY)");
         $("#main").text(response.name + " " + today);
         $("#temp").text("Temperature: " + Math.ceil(response.main.temp) + " F");
         $("#humidity").text("Humidity: " + response.main.humidity + "%");
         $("#wind").text("Wind Speed: " + response.wind.speed + " MPH");
+
+        var weatherIcon = $("<img>");
+        var iconURL = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        weatherIcon.attr("src", iconURL);
+        $("#main").append(weatherIcon);
 
 
         var latitude = response.coord.lat;
@@ -33,8 +38,6 @@ function getUVI(lat, lon){
         url: "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey,
         method: "GET"
     }).then(function(response){
-        console.log(response);
-
         var span = $("<span>");
         span.text(response.value);
         if(response.value < 3){
@@ -58,21 +61,26 @@ function getForecast(city){
         url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey,
         method: "GET"
     }).then(function(response){
-        console.log(response);
         for(let i = 0; i < forecast.length; i++){
             var day = moment().add(i + 1, "days").format("MM/D/YYYY");
             $(forecast[i]).text(day);
 
-            var newDivT = $("<div>").text("Temperature: " + Math.ceil(response.list[i].main.temp));
+            var weatherIcon = $("<img>");
+            var iconURL = "http://openweathermap.org/img/w/" + response.list[index[i]].weather[0].icon + ".png";
+            weatherIcon.attr("src", iconURL);
+            $(forecast[i]).append(weatherIcon);            
+
+            var newDivT = $("<div>").text("Temperature: " + Math.ceil(response.list[index[i]].main.temp));
             $(forecast[i]).append(newDivT);
 
-            var newDivH = $("<div>").text("Humidity: " + response.list[i].main.humidity + "%");
+            var newDivH = $("<div>").text("Humidity: " + response.list[index[i]].main.humidity + "%");
             $(forecast[i]).append(newDivH);
         }
 
     });
 }
 
-var chicago = "chicago";
-getWeather(chicago)
-getForecast(chicago);
+$(document).on("click", ".cities", function(){
+    getWeather($(this).text());
+    getForecast($(this).text());
+});
